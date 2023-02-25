dd6d962df75498763b987a4ef903054929b44ee0
yashnayi234 <37475613+yashnayi234@users.noreply.github.com>
2023-02-25 17:39:37 +0530

    preload feature

---
+++ assets/js/preloader.js
@@ -0,0 +1,10 @@
+function redirect () {
+    var interval = setInterval(myURL, 6000);
+    var result = document.getElementById("loader");
+    // result.innerHTML = "<b> The page will redirect after delay of 5 seconds setInterval() method.";
+ }
+
+ function myURL() {
+    document.location.href = 'index.html';
+    clearInterval(interval);
+ }
